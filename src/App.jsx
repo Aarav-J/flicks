c6a555f4f7469b4ebb4f43b323d008dcf14642e9
Aarav Jain <aaravjain2007@gmail.com>
2022-06-23 21:26:33 -0400

    Made Card & Alert & Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,113 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import React from "react";
 import axios from "axios";
 import Movie from "./components/Movie";
+// import Alert from "./components/Alert";
+import {
+  Grid,
+  GridItem,
+  Flex,
+  Input,
+  IconButton,
+  useDisclosure,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
+  const [open, setOpen] = useState(false);
   const [input, setInput] = useState("");
   const [movies, setMovies] = useState([]);
+  const cancelRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const handleSubmit = async () => {
     const url = `http://www.omdbapi.com/`;
     const queryString = { apikey: "7f921e04", t: input };
     const response = await axios.get(url, { params: queryString });
+
     const data = response.data;
-
-    const newMovie = {
-      Title: data.Title,
-      Year: data.Year,
-      Runtime: data.Runtime,
-      Plot: data.Plot,
-      Image: data.Poster,
-      Status: "Watching",
-    };
-    setMovies([...movies, newMovie]);
-
+    console.log(data);
+    if (data.Response !== "False") {
+      const newMovie = {
+        Title: data.Title,
+        Year: data.Year,
+        Runtime: data.Runtime,
+        Plot: data.Plot,
+        Image: data.Poster,
+        Status: "Watching",
+      };
+      console.log(open);
+      setMovies([...movies, newMovie]);
+    } else {
+      onOpen();
+    }
     setInput("");
   };
   return (
     <div>
-      <input
-        type="search"
-        placeholder="Search for movie"
-        value={input}
-        onChange={(event) => {
-          setInput(event.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.key == "Enter") {
-            handleSubmit();
-          }
-        }}
-      />
-
-      <button
-        onClick={() => {
-          handleSubmit();
-        }}
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
       >
-        Submit
-      </button>
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Movie Not Found
+            </AlertDialogHeader>
 
-      {movies.map((movie) => {
-        return <Movie movie={movie} />;
-      })}
+            <AlertDialogBody>
+              Movie was not in the database. Check your spelling or if that
+              movie exists.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} colorScheme="green" onClick={onClose}>
+                Ok
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+
+      <Flex mr="5" ml="5" mt="5">
+        <Input
+          variant="outline"
+          placeholder="Search for movie"
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSubmit();
+            }
+          }}
+        />
+        <IconButton
+          aria-label="Search database"
+          icon={<SearchIcon />}
+          onClick={() => {
+            handleSubmit();
+          }}
+        />
+      </Flex>
+      <Grid templateColumns="repeat(3,1fr)" gap={8}>
+        {movies.map((movie) => {
+          return (
+            <GridItem mt="5" ms="5" me="5">
+              <Movie movie={movie} />
+            </GridItem>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,23 +1,48 @@
-import Card from "react-bootstrap/Card";
-
+// import Card from "react-bootstrap/Card";
+import { Badge, Box, Image } from "@chakra-ui/react";
+// import { StarIcon } from "@chakra-ui/icons";
 const Movie = (movie) => {
-  console.log("movie:", movie);
   return (
-    <div>
-      <Card style={{ width: "18rem" }} className="movie" bg="light">
-        <Card.Img variant="top" src={movie.movie.Image} className="image" />
-        <Card.Body>
-          <Card.Title>
-            {movie.movie.Title}({movie.movie.Year})
-          </Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">
-            {movie.movie.Runtime}
-          </Card.Subtitle>
+    <Box
+      maxW="sm"
+      maxH="45em"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="scroll"
+      as="button"
+    >
+      <Image boxSize="100%" src={movie.movie.Image} />
 
-          <Card.Text>{movie.movie.Plot}</Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
+      <Box p="6">
+        <Box display="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="2" colorScheme="orange">
+            {movie.movie.Status}
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="xs"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {movie.movie.Runtime}
+          </Box>
+        </Box>
+
+        <Box
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          lineHeight="tight"
+          noOfLines={2}
+        >
+          {movie.movie.Title}({movie.movie.Year})
+        </Box>
+
+        <Box>{movie.movie.Plot}</Box>
+      </Box>
+    </Box>
   );
 };
 
